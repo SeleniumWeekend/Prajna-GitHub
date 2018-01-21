e9820134248c5eb04310874db54062a7688e35b7
user <user@user-PC>
2018-01-21 14:23:26 +0530

    Commit

---
+++ GitProject/src/GitPackage/GitClass.java
@@ -0,0 +1,15 @@
+package GitPackage;
+
+public class GitClass {
+	public static void m1() {
+		System.out.println("Running m1()");
+		}
+		
+		
+
+	public static void main(String[] args) {
+		GitClass.m1();
+
+	}
+
+}
